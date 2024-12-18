96e4ae4cf568d5920e86783f4aef3eb01b133116
Maciej-krol71 <01197815@pw.edu.pl>
2024-12-18 13:51:37 +0100

    Zaimplementowano eliminacje Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -4,10 +4,43 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
+void switchrows(Matrix *mat, int j, int l) {
+	double * tmp = mat->data[j];
+	mat->data[j] = mat->data[l];
+	mat->data[l] = tmp;
+}
+
+double find_max(Matrix *mat, int j) {
+	int i;
+	int l = j;
+        double max = mat->data[j][j];
+        for (i=j; i<mat->r; i++) {
+		if(mat->data[i][j] > max) {
+			max = mat->data[i][j];
+			l = i;
+		}
+	}
+	switchrows(mat, j, l);
+}
+
+void redukcja(Matrix *mat, Matrix *b, double dziel, int i, int f) {
+	int h;
+	for (h=0; h<mat->c; h++) {
+		mat->data[f][h] = mat->data[f][h] - mat->data[i][h] * dziel;
+	}
+	b->data[f][0] = b->data[f][0] - b->data[i][0] * dziel;
+}
+
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+    	int i;
+	for (i=0; i<mat->r; i++) {
+		find_max(mat, i);
+		int k;
+		for(k=1; k<mat->r-i; k++) {
+			redukcja(mat, mat->data[i+k][i] / mat->data[i][i], i, i+k);
+		}
+	}
 
 		return 0;
 }
--- src/gauss.h
+++ src/gauss.h
@@ -7,6 +7,10 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
+void switchrows(Matrix * mat, int j, int l);
+double find_max(Matrix *mat, int j);
+void redukcja(Matrix *mat, Matrix *b, double dziel, int i, int f);
 int eliminate(Matrix *mat, Matrix *b);
 
 #endif
